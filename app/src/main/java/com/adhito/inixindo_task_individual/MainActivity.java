d57f698e927a9bb4e1f3c718d162a62b4a5d2eae
Adhito <adhito909@gmail.com>
2022-02-08 17:49:43 +0700

    Added Home and Form Fragments

--- app/src/main/java/com/adhito/inixindo_task_individual/MainActivity.java
+++ app/src/main/java/com/adhito/inixindo_task_individual/MainActivity.java
@@ -1,14 +1,106 @@
 package com.adhito.inixindo_task_individual;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.adhito.inixindo_task_individual.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        // Custom Toolbar
+        setSupportActionBar(binding.toolbar);
+
+        // Default fragment yang dibuka pertama kali. (menu pada saat aplikasi dibuka)
+        getSupportActionBar().setTitle("Home");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, new HomeFragment())
+                .commit();
+        binding.navView.setCheckedItem(R.id.nav_home);
+
+        // Drawer Open
+        toggle = new ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, R.string.open, R.string.close);
+
+        // Drawer BackButton
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+        // Drawer Syncronization
+        toggle.syncState();
+
+        // Menu navigasi dipilih
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+
+            // Pindah ke fragment saat dipilih
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        getSupportActionBar().setTitle("Home");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+
+                    case R.id.nav_form:
+                        fragment = new FormFragment();
+                        getSupportActionBar().setTitle("Form Fragment");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navView);
+        View headerView = getLayoutInflater().inflate(R.layout.nav_header_layout, navigationView, false);
+        navigationView.addHeaderView(headerView);
+
+    }
+
+    private void callFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+        );
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
