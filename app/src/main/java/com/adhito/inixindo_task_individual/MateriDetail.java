e1620afd352299bfb8b0fc8dab5aeb7ef2b0c837
Adhito <adhito909@gmail.com>
2022-02-10 12:12:48 +0700

    Added Materi CRUD logic at MateriDetail, MateriTambah class

--- app/src/main/java/com/adhito/inixindo_task_individual/MateriDetail.java
+++ app/src/main/java/com/adhito/inixindo_task_individual/MateriDetail.java
@@ -1,14 +1,254 @@
 package com.adhito.inixindo_task_individual;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MateriDetail extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class MateriDetail extends AppCompatActivity implements View.OnClickListener {
+    
+    EditText edit_id_mat, edit_nama_mat;
+    Button btn_update_materi, btn_delete_materi;
+    String id_mat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materi_detail);
+
+        edit_id_mat = findViewById(R.id.edit_id_mat);
+        edit_nama_mat = findViewById(R.id.edit_nama_mat);
+        btn_update_materi = findViewById(R.id.btn_update_materi);
+        btn_delete_materi = findViewById(R.id.btn_delete_materi);
+
+        Intent receiveIntent = getIntent();
+        id_mat = receiveIntent.getStringExtra(Konfigurasi.PGW_ID);
+        edit_id_mat.setText(id_mat);
+
+        getJSON();
+
+        btn_update_materi.setOnClickListener(this);
+        btn_delete_materi.setOnClickListener(this);
+    }
+
+    private void getJSON() {
+
+        // Get peserta data from MySQL throught Web-API with JSON format
+        class GetJSON extends AsyncTask<Void, Void, String> {
+            ProgressDialog loading;
+
+            // Override PreExecute (Ctrl + O select the onPreExecute)
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(
+                        MateriDetail.this,
+                        "Mengambil data materi",
+                        "Harap Menunggu ...",
+                        false,
+                        false);
+            }
+
+            // Override doInBackground (Ctrl + O select the doInBackground)
+            @Override
+            protected String doInBackground(Void... voids) {
+                HttpHandler handler = new HttpHandler();
+                String result = handler.sendGetResponse(Konfigurasi.URL_MATERI_GET_DETAIL, id_mat);
+                Log.d("Result MateriDetail", result);
+                return result;
+            }
+
+            // Override onPostExecute (Ctrl + O select the onPostExecute)
+            @Override
+            protected void onPostExecute(String message) {
+                super.onPostExecute(message);
+                loading.dismiss();
+                displayDetailData(message);
+            }
+        }
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+    private void displayDetailData(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray result = jsonObject.getJSONArray(Konfigurasi.TAG_JSON_ARRAY);
+            JSONObject object = result.getJSONObject(0);
+
+            String nama_mat = object.getString("nama_mat");
+
+            edit_nama_mat.setText(nama_mat);
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btn_update_materi){
+            confirmUpdateMateri();
+        } else if (view == btn_delete_materi){
+            confirmDeleteMateri();
+        }
+    }
+
+    private void confirmUpdateMateri() {
+        // Show confirmation alert dialogue
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Memperbarui data materi");
+        builder.setMessage("Apakah anda ingin memperbarui materi ini ?");
+        builder.setIcon(getResources().getDrawable(android.R.drawable.ic_input_add));
+        builder.setCancelable(false);
+        builder.setNegativeButton("Cancel", null);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                updateDataMateri();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void updateDataMateri() {
+        // Define variables that is used
+        final String nama_mat = edit_nama_mat.getText().toString().trim();
+
+        class UpdateDataMateri extends AsyncTask<Void, Void, String>{
+            ProgressDialog loading;
+
+            // Override onPreExecute (Ctrl + O select the onPreExecute)
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(
+                        MateriDetail.this,
+                        "Memperbarui data",
+                        "Harap tunggu ...",
+                        false,
+                        false);
+            }
+
+            // Override doInBackground (Ctrl + O select the doInBackground)
+            @Override
+            protected String doInBackground(Void... voids) {
+                HashMap<String, String> materi = new HashMap<>();
+                materi.put("id_mat", id_mat);
+                materi.put("nama_mat", nama_mat);
+
+                HttpHandler handler = new HttpHandler();
+                String result = handler.sendPostRequest(Konfigurasi.URL_MATERI_UPDATE, materi)  ;
+                return result;
+            }
+
+            // Override onPostExecute (Ctrl + O select the onPostExecute)
+            @Override
+            protected void onPostExecute(String message) {
+                super.onPostExecute(message);
+                loading.dismiss();
+                Toast.makeText(
+                        MateriDetail.this,
+                        "Pesan InstrukturDetail:"+message,
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+                // Back to homepage after update
+                startActivity(new Intent(MateriDetail.this,MainActivity.class));
+            }
+        }
+        UpdateDataMateri updateDataMateri = new UpdateDataMateri();
+        updateDataMateri.execute();
+    }
+
+
+    private void confirmDeleteMateri() {
+        // Show confirmation alert dialogue
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Menghapus data : ");
+        builder.setMessage("Apakah anda ingin menghapus materi ini ?");
+        builder.setIcon(getResources().getDrawable(android.R.drawable.ic_delete));
+        builder.setCancelable(false);
+        builder.setNegativeButton("Cancel", null);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteDataMateri();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void deleteDataMateri() {
+        class DeleteDataMateri extends AsyncTask<Void, Void, String>{
+            ProgressDialog loading;
+
+            // Override onPreExecute (Ctrl + O select the onPreExecute)
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(
+                        MateriDetail.this,
+                        "Menghapus data",
+                        "Harap tunggu",
+                        false,
+                        false);
+            }
+
+            // Override doInBackground (Ctrl + O select the doInBackground)
+            @Override
+            protected String doInBackground(Void... voids) {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("id_mat", id_mat);
+
+                HttpHandler handler = new HttpHandler();
+                String result = handler.sendGetResponse(Konfigurasi.URL_MATERI_DELETE, id_mat);
+                Log.d("result",result);
+                return result;
+            }
+
+            // Override onPostExecute (Ctrl + O select the onPostExecute)
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(
+                        MateriDetail.this,
+                        "Pesan Delete: "+s,
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+                // Back to homepage after update
+                startActivity(new Intent(MateriDetail.this,MainActivity.class));
+
+            }
+        }
+        DeleteDataMateri deleteDataMateri = new DeleteDataMateri();
+        deleteDataMateri.execute();
     }
 }
--- app/src/main/java/com/adhito/inixindo_task_individual/MateriTambah.java
+++ app/src/main/java/com/adhito/inixindo_task_individual/MateriTambah.java
@@ -1,14 +1,136 @@
 package com.adhito.inixindo_task_individual;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MateriTambah extends AppCompatActivity {
+import com.adhito.inixindo_task_individual.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
+
+public class MateriTambah extends AppCompatActivity implements View.OnClickListener {
+    private ActivityMainBinding binding;
+    EditText edit_id_materi, edit_nama_mat;
+    Button btn_tambah_materi, btn_lihat_materi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materi_tambah);
+
+        edit_id_materi = findViewById(R.id.edit_id_mat);
+        edit_nama_mat = findViewById(R.id.edit_nama_mat);
+        btn_tambah_materi = findViewById(R.id.btn_tambah_materi);
+        btn_lihat_materi = findViewById(R.id.btn_lihat_materi);
+        btn_tambah_materi.setOnClickListener(this);
+        btn_lihat_materi.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Fragment fragment = null;
+        switch (view.getId()) {
+            case R.id.btn_tambah_materi:
+                confirmAddDataInstruktur();
+                break;
+            case R.id.btn_lihat_materi:
+                fragment = new PesertaFragment();
+                startActivity(new Intent(MateriTambah.this, MainActivity.class));
+                break;
+        }
+    }
+
+    private void confirmAddDataInstruktur() {
+        // Get confirmation to add participant (Name, E-mail, Phone and Instance)
+        final String nama_mat = edit_nama_mat.getText().toString().trim();
+
+        // Show confirmation of alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Menambahkan data materi");
+        // builder.setMessage("Apakah anda ingin menambah peserta ini ? ");
+        builder.setMessage("Apakah anda ingin menambah materi ini ?" +
+                "\n Nama \t\t\t: " + nama_mat);
+        builder.setIcon(getResources().getDrawable(android.R.drawable.ic_input_add));
+        builder.setCancelable(false);
+        builder.setNegativeButton("Cancel", null);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                simpanDataMateri();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void simpanDataMateri() {
+        final String nama_mat = edit_nama_mat.getText().toString().trim();
+
+        class SimpanDataMateri extends AsyncTask<Void, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MateriTambah.this,
+                        "Menyimpan Data",
+                        "Harap Tunggu ...",
+                        false,
+                        false);
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                // Create hashmap to store values which will be sent to HttpHandler
+                HashMap<String, String> params = new HashMap<>();
+                params.put("nama_mat", nama_mat);
+                HttpHandler handler = new HttpHandler();
+
+                // Create HttpHandler to send data with sendPostRequest
+                String result = handler.sendPostRequest(Konfigurasi.URL_MATERI_ADD, params);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MateriTambah.this, "Pesan:" + s,
+                        Toast.LENGTH_SHORT).show();
+                clearText();
+            }
+        }
+        SimpanDataMateri simpanDataMateri = new SimpanDataMateri();
+        simpanDataMateri.execute();
+        startActivity(new Intent(MateriTambah.this, MainActivity.class));
+    }
+
+    private void callFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+        );
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void clearText() {
+        edit_nama_mat.setText("");
     }
 }
