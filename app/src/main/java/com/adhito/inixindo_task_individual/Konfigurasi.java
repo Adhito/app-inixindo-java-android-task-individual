a815834bdcc21e7eef24597462638b997558f68b
Adhito <adhito909@gmail.com>
2022-02-10 11:05:34 +0700

    Added MateriFragment, MateriDetail, MateriTambah logic and layout initial commit

--- app/src/main/java/com/adhito/inixindo_task_individual/Konfigurasi.java
+++ app/src/main/java/com/adhito/inixindo_task_individual/Konfigurasi.java
@@ -4,9 +4,6 @@ package com.adhito.inixindo_task_individual;
 public class Konfigurasi {
     // API URL untuk GET, GET_DETAIL, ADD, UPDATE dan DELETE
 
-    // Web API untuk detail_kelas
-    public static final String URL_DETAIL_KELAS_GET_ALL     = "http://192.168.5.83/api_task_individual/get_all_detail_kelas.php";
-
     // Web API untuk peserta
     public static final String URL_PESERTA_GET_ALL       = "http://192.168.5.83/api_task_individual/get_all_peserta.php";
     public static final String URL_PESERTA_GET_DETAIL    = "http://192.168.5.83/api_task_individual/get_detail_peserta.php?id_pst=";
@@ -21,6 +18,13 @@ public class Konfigurasi {
     public static final String URL_INSTRUKTUR_UPDATE        = "http://192.168.5.83/api_task_individual/update_instruktur.php";
     public static final String URL_INSTRUKTUR_DELETE        = "http://192.168.5.83/api_task_individual/delete_instruktur.php?id_ins=";
 
+    // Web API untuk materi
+    public static final String URL_MATERI_GET_ALL       = "http://192.168.5.83/api_task_individual/get_all_materi.php";
+    public static final String URL_MATERI_GET_DETAIL    = "http://192.168.5.83/api_task_individual/get_detail_materi.php?id_mat=";
+    public static final String URL_MATERI_ADD           = "http://192.168.5.83/api_task_individual/add_materi.php";
+    public static final String URL_MATERI_UPDATE        = "http://192.168.5.83/api_task_individual/update_materi.php";
+    public static final String URL_MATERI_DELETE        = "http://192.168.5.83/api_task_individual/delete_materi.php?id_mat=";
+
     // key adn value JSON yang muncul di browser
     public static final String KEY_PGW_ID = "id";
     public static final String KEY_PGW_NAMA = "name";
@@ -31,6 +35,7 @@ public class Konfigurasi {
     public static final String TAG_JSON_ARRAY = "result";
     public static final String TAG_JSON_ID = "id_pst";
     public static final String TAG_JSON_ID_INS = "id_ins";
+    public static final String TAG_JSON_ID_MAT = "id_mat";
     public static final String TAG_JSON_NAMA ="name";
     public static final String TAG_JSON_JABATAN ="desg";
     public static final String TAG_JSON_GAJI ="salary";
